99aaeb793c2b73a409981f212defc550f00a4424
AnneKaPunkt <annekerb@icloud.com>
2026-05-26 13:07:59 +0200

    refactor: Frage in Dictionary Struktur umgewandelt
    - Struktur vom reinen Text auf einem Dictionary (Schlüssel-Wert-Paare) umgestellt.
    - Trennung von Daten und Logik vorbereitet.
    - Dynamische Abfrage der korrekten Lösung über den Schlüssel 'loesung'.

--- quiz.py
+++ quiz.py
@@ -2,18 +2,37 @@ def main():
     print("=== Wilkommen beim IT_Fachbegriffe-Quiz ===")
     print("Teste dein Wissen!\n")
 
-    # Eine Frage auf dem Bildschirm anzeigen
-    print("Frage 1: Was bedeutet die Abkürzung 'RAM'?")
-    print("A) Read Access Memory\nB) Random Access Memory")
+    # Ein Wörterbuch (Dictionary) für die Frage erstellen
+    frage_data = {
+        "text" : "Was bedeutet die Abkürzung 'RAM'?",
+        "optionen" : "A) Read Access Memory\nB) Random Access Memory",
+        "loesung" : "B"
+    } 
+
+    # Die Daten aus dem Dictionary über die Schlüssel (Keys) anzeigen
+    print(frage_data["text"])
+    print(frage_data["optionen"])
+
+    # # Eine Frage auf dem Bildschirm anzeigen
+    # print("Frage 1: Was bedeutet die Abkürzung 'RAM'?")
+    # print("A) Read Access Memory\nB) Random Access Memory")
 
     # Eingabe vom Nutzer holen, Leerzeichen entfernen (.strip) und in Großbuchstaben umwandeln (.upper)
     antwort = input("Deine Antwort (A oder B):").strip().upper()
 
     # Die Antwort überprüfen
-    if antwort == "B":
+    # if antwort == "B":
+    #     print("Yüpppiiieee! Richtig :) RAM steht für Random Access Memory")
+    # else:
+    #     print("Nöpedi...nö! Leider falsch :( RAM steht für Random Access Memory")
+
+    # Wir prüfen direkt gegen den Wert hinter dem Schlüssel "loesung"
+
+    if antwort == frage_data["loesung"]:
         print("Yüpppiiieee! Richtig :) RAM steht für Random Access Memory")
     else:
-        print("Nöpedi...nö! Leider falsch :( RAM steht für Random Access Memory")
+         print(f"Nöpedi...nö! Leider falsch. :( Die richtige Antwort ist {frage_data['loesung']} (Random Access Memory).")
+        
 
 
 if __name__ == "__main__":
