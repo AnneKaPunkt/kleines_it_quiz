90d706781e77165d06bc736a1066007c510a9ca0
AnneKaPunkt <annekerb@icloud.com>
2026-05-26 12:02:43 +0200

    feat: Fragenpool und For-Schleife für Dynamischen Spielablauf
    - Einrückungsfehler in der Hauptfunktion  behoben.
    - Benutzereingabe mit input() integriert
    - Eingabe-Validierung mittels .strip().upper() hinzugefügt, um unempfindlich gegen Leerzeichen und Kleinschreibung zu sein.
    - Erste funktionierende Testläufe im Terminal absolviert

--- quiz.py
+++ quiz.py
@@ -2,5 +2,19 @@ def main():
     print("=== Wilkommen beim IT_Fachbegriffe-Quiz ===")
     print("Teste dein Wissen!\n")
 
-    if __name__ == "__main__":
-        main()
+    # Eine Frage auf dem Bildschirm anzeigen
+    print("Frage 1: Was bedeutet die Abkürzung 'RAM'?")
+    print("A) Read Access Memory\nB) Random Access Memory")
+
+    # Eingabe vom Nutzer holen, Leerzeichen entfernen (.strip) und in Großbuchstaben umwandeln (.upper)
+    antwort = input("Deine Antwort (A oder B):").strip().upper()
+
+    # Die Antwort überprüfen
+    if antwort == "B":
+        print("Yüpppiiieee! Richtig :) RAM steht für Random Access Memory")
+    else:
+        print("Nöpedi...nö! Leider falsch :( RAM steht für Random Access Memory")
+
+
+if __name__ == "__main__":
+    main()
